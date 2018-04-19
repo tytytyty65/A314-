19634220b9402564d0ec5059c374f8636fab9b1c
范博勝 <tytytyty65@icloud.com>
2018-04-20 01:11:36 +0800

    鐵板神算

--- A314鐵板神算/ViewController.swift
+++ A314鐵板神算/ViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var qna: UITextField!
+    
+    @IBOutlet weak var ans: UILabel!
+    
+    var word:String = ""
+    
+    @IBAction func sendout(_ sender: UIButton) {
+        word = qna.text!
+        
+        if word.hasPrefix("生命"){
+        ans.text = "種種證據顯示，就是巧克力"
+            //發音的程式碼
+            let speechUtterance = AVSpeechUtterance(string: "種種證據顯示，就是巧克力")
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+            let synth = AVSpeechSynthesizer()
+            synth.speak(speechUtterance)
+        }
+        else if word.hasSuffix("有神嗎"){
+        ans.text = "我真的沒有準備來回答這些問題"
+            //發音的程式碼
+            let speechUtterance = AVSpeechUtterance(string: "我真的沒有準備來回答這些問題")
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+            let synth = AVSpeechSynthesizer()
+            synth.speak(speechUtterance)
+        }
+        else if word.contains("笑話"){
+        ans.text = "我不會，我老是破梗"
+            //發音的程式碼
+            let speechUtterance = AVSpeechUtterance(string: "我不會，我老是破梗")
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+            let synth = AVSpeechSynthesizer()
+            synth.speak(speechUtterance)
+        }
+        else{
+        ans.text = "我不知道"
+            //發音的程式碼
+            let speechUtterance = AVSpeechUtterance(string: "我不知道")
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+            let synth = AVSpeechSynthesizer()
+            synth.speak(speechUtterance)
+        }
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
